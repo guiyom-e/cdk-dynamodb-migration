12689d6be20f87ad0e16420139bb0be9ba65a31a
guiyom-e <guillaume.egee@outlook.com>
2023-07-21 17:58:11 +0200

    renaming

--- cdk-dynamodb-migrator/lib/cdk-stack.ts
+++ cdk-dynamodb-migrator/lib/cdk-stack.ts
@@ -2,7 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { BillingMode, Table, AttributeType } from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
 
-export class CdkStack extends cdk.Stack {
+export class DynamoDBMigratorStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
