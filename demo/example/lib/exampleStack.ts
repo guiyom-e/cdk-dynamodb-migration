07b270bd61cd410bc7550b8624ec650c2e0ec403
gozineb <zinebe@theodo.fr>
2023-10-06 17:03:14 +0200

    🔊 update policy to have logs

--- demo/example/lib/exampleStack.ts
+++ demo/example/lib/exampleStack.ts
@@ -1,8 +1,8 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb';
-import { PolicyStatement, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
-import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { Architecture, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { RetentionDays } from 'aws-cdk-lib/aws-logs';
 import { MigrationConstruct } from 'cdk-dynamodb-migrator';
 import { Construct } from 'constructs';
 import path from 'path';
@@ -12,23 +12,22 @@ export class ExampleStack extends Stack {
     super(scope, id, props);
 
     const dinosaursTable = new Table(this, 'DinosaursTable', {
-      partitionKey: { name: 'DinosaurID', type: AttributeType.STRING },
+      partitionKey: { name: 'dinosaurId', type: AttributeType.STRING },
     });
 
-    const lambdaRole = new Role(this, 'MigrationLambdaRole', {
-      assumedBy: new ServicePrincipal('lambda.amazonaws.com'),
-    });
-
-    lambdaRole.addToPolicy(
-      new PolicyStatement({
-        actions: [
-          'dynamodb:GetItem',
-          'dynamodb:UpdateItem',
-          'dynamodb:DeleteItem',
-        ],
-        resources: [dinosaursTable.tableArn],
-      }),
+    const migrationLambdaFunction = new NodejsFunction(
+      this,
+      'MigrationLambdaFunction',
+      {
+        runtime: Runtime.NODEJS_18_X,
+        handler: 'index.handler',
+        entry: path.join(__dirname, 'runMigrationsLambda/index.ts'),
+        logRetention: RetentionDays.ONE_WEEK,
+        architecture: Architecture.ARM_64,
+      },
     );
+
+    dinosaursTable.grantReadWriteData(migrationLambdaFunction);
 
     new MigrationConstruct(this, 'DynamoDBMigrator', {
       configuration: {
@@ -42,7 +41,6 @@ export class ExampleStack extends Stack {
               runtime: Runtime.NODEJS_18_X,
               handler: 'index.handler',
               entry: path.join(__dirname, 'runMigrationsLambda/index.ts'),
-              role: lambdaRole,
             },
           ),
         },
